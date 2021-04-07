20d10106642a35812149cd81cbbf2ac3344e46d9
Janne-R <74651035+Janne-R@users.noreply.github.com>
2021-04-07 14:12:50 +0200

    done with fasit

--- js/script.js
+++ js/script.js
@@ -30,3 +30,25 @@ button.onclick = function () {
   button.disabled = true;
 }
 
+//fasit
+let counter = 0;
+const button = document.querySelector("button");
+const countContainer = document.querySelector(".count");
+
+button.onclick = function () {
+  event.target.disabled = true;
+
+  // reset counter
+  counter = 0;
+
+  const intervalId = setInterval(function () {
+    counter++; // add 1 to counter
+    countContainer.innerHTML = counter;
+
+    if (counter === 7) {
+      clearInterval(intervalId);
+      button.disabled = false;
+    }
+  }, 1000);
+};
+
